756cf559365af05f90bc285650e7b40a2736022a
aviraltuteja <aviraltuteja@gmail.com>
2025-08-08 14:10:32 +0530

    removed vestegial

--- app/new-design/page.tsx
+++
@@ -1,9 +0,0 @@
-import { StaticCanvas, FabricText } from "fabric";
-const canvas = new StaticCanvas();
-const helloWorld = new FabricText("Hello world!");
-canvas.add(helloWorld);
-canvas.centerObject(helloWorld);
-
-export default function NewDesign(): React.ReactElement {
-  return <div></div>;
-}
